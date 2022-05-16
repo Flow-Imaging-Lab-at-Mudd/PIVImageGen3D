4626de0a577c799cbe2889d91dd942c4f8456fd8
unknown <leah.r.mendelson@gmail.com>
2022-05-16 15:27:47 -0700

    Add 3D working files

---
+++ src/MATLAB/GenerateTestImages3D.m
@@ -0,0 +1,44 @@
+%This program generates synthetic PIV images and exports validation data.
+%Copyright (C) 2019  Luís Mendes, Prof. Rui Ferreira, Prof. Alexandre Bernardino
+%
+%This program is free software; you can redistribute it and/or
+%modify it under the terms of the GNU General Public License
+%as published by the Free Software Foundation; either version 2
+%of the License, or (at your option) any later version.
+%
+%This program is distributed in the hope that it will be useful,
+%but WITHOUT ANY WARRANTY; without even the implied warranty of
+%MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%GNU General Public License for more details.
+%
+%You should have received a copy of the GNU General Public License
+%along with this program; if not, write to the Free Software
+%Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+%
+%Special thanks go to Ana Margarida and Rui Aleixo
+%and their initial effort on building a draft for a similar tool.
+
+clear *; clear GLOBAL *;
+close all;
+
+sizeX=512; %Image width without margins
+sizeY=512; %Image height without margins
+
+displayFlowField=true; %Display image of each flow field,
+closeFlowField=true; %and close it automatically
+
+%flows={'rk_uniform' 'rankine_vortex' 'parabolic' 'uniform' 'stagnation',...
+%        'shear', 'shear_22d3', 'shear_45d0', 'decaying_vortex'};
+flows={'rankine_vortex'};
+
+bitDepths=8; % leave at 8 bits for all tests
+deltaXFactor=[0.05 0.25]; % max. displacement as a fraction of the final window size
+particleRadius=[1.5 3]; % in pixels
+Ni=[6 10]; % # of particles in each window
+noiseLevel=0; % turn off noise for now
+outOfPlaneStdDeviation=0; % turn off out of plane motion for now
+numberOfRuns=1;
+winSize = [16 32]; % interrogation window sizes
+sheetThickness = [10 50]; % light sheet thickness in mm
+
+generate3DPIVImagesAllCombinations;
